47f3fad3accd44d5c61999fc70042dba1e0e3a1b
derekleeck <ckdh1704@gmail.com>
2022-07-05 18:16:38 +0900

    수정: shop merge request

--- src/main/java/shop/controller/ShopController.java
+++ src/main/java/shop/controller/ShopController.java
@@ -1,22 +1,24 @@
-package main.controller;
+package shop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
-public class MainController {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView home() {
+@Controller
+public class ShopController {
+	
+	@RequestMapping(value = "/shop/shopindex", method = RequestMethod.GET)
+	public ModelAndView shop() {
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("menu", "/WEB-INF/views/main/menu.jsp");
-		mav.addObject("main", "/WEB-INF/views/main/main.jsp");
 		mav.addObject("footer", "/WEB-INF/views/main/footer.jsp");
-		mav.setViewName("/index");
+		mav.addObject("shopmain", "/WEB-INF/views/shop/shopmain.jsp");
+		mav.addObject("shopmenu", "/WEB-INF/views/shop/shopmenu.jsp");
+		mav.setViewName("/shop/shopindex");
 		
 		return mav;
 	}
-	
 }
+
