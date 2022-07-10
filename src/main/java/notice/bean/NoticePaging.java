e1af84475a68c8b66d286338b9d7e915c4aa2794
yeonsubak <91791222+yeonsubak@users.noreply.github.com>
2022-07-10 13:46:36 +0900

    #106 NoticePaging.java: 본인 수정코드가 아님으로 main 코드로 변경
    + 인코딩 문제로 한글 깨짐

--- src/main/java/notice/bean/NoticePaging.java
+++ src/main/java/notice/bean/NoticePaging.java
@@ -7,27 +7,25 @@ import lombok.Data;
 @Component
 @Data
 public class NoticePaging {
-	private int currentPage; //�쁽�옱�럹�씠吏�
-	private int pageBlock; //[�씠�쟾][1][2][3][�떎�쓬]
-	private int pageSize; //1�럹�씠吏��떦 10媛쒖뵫
-	private int totalA; //珥앷��닔
+	private int currentPage; //현재페이지
+	private int pageBlock; //[이전][1][2][3][다음]
+	private int pageSize; //1페이지당 10개씩
+	private int totalA; //총글수
 	private StringBuffer pagingHTML;
-	
-	
 	
 	public void makePagingHTML() {
 		pagingHTML = new StringBuffer();
 		
-		//int totalP = (totalA-1) / pageSize + 1; //珥� �럹�씠吏� �닔
+		//int totalP = (totalA-1) / pageSize + 1; //총 페이지 수
 		int totalP = (totalA + pageSize-1) / pageSize;
 		
-		int startPage = (currentPage-1) / pageBlock * pageBlock + 1; //�떆�옉 �럹�씠吏� 踰덊샇
+		int startPage = (currentPage-1) / pageBlock * pageBlock + 1; //시작 페이지 번호
 		
 		int endPage = startPage + pageBlock - 1;
 		if(endPage > totalP) endPage = totalP;
 		
-		if(startPage > pageBlock) //if(startPage != 1) �몮 �떎 媛��뒫
-			pagingHTML.append("[ <span id='paging' onclick='noticePaging("+ (startPage-1) + ")'>�씠�쟾</span> ]");
+		if(startPage > pageBlock) //if(startPage != 1) 둘 다 가능
+			pagingHTML.append("[ <span id='paging' onclick='noticePaging("+ (startPage-1) + ")'>이전</span> ]");
 		
 		for(int i=startPage; i<=endPage; i++) {
 			if(i == currentPage) 
@@ -37,7 +35,7 @@ public class NoticePaging {
 		}
 		
 		if(endPage < totalP)
-			pagingHTML.append("[ <span id='paging' onclick='noticePaging("+ (endPage+1) + ")'>�떎�쓬</span> ]");
+			pagingHTML.append("[ <span id='paging' onclick='noticePaging("+ (endPage+1) + ")'>다음</span> ]");
 
 	}
 
