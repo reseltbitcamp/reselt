48cd45da49f94f94df2b6c65ec117d4882488f15
derekleeck <ckdh1704@gmail.com>
2022-07-19 23:31:11 +0900

    187-db-middlesave

--- src/main/java/shop/bean/ProductInfoDTO.java
+++ src/main/java/shop/bean/ProductInfoDTO.java
@@ -12,6 +12,11 @@ public class ProductInfoDTO {
 	private String product_name_kor;
 	private String product_name_eng;
 	private String product_id;
+	private String product_image;
+	private String brand_firstname;
+	private String brand_lastname;
+	private int gender_id;
+	private int brand_id;
 	private int category_id;
 	private int released_price;
 	private Date created_at;
