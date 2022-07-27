0fb63232f408e39b1ead083c41faa5d4cb1c57f9
derekleeck <ckdh1704@gmail.com>
2022-07-27 16:09:02 +0900

    save me pls

--- src/main/webapp/assets/js/shop/shopMain.js
+++ src/main/webapp/assets/js/shop/shopMain.js
@@ -31,22 +31,24 @@ function ProductList(){
 	        	else if (priceRange >500000){priceRange = 'highprice'};
 
                //data-category="green small medium africa"
-               $('<div class="product" data-category="'
+               $('<div class="product item" data-category="'
                +items.category_name_eng+' '
                +items.brand_name+' '
-               +items.gender_name+'"><button type="button"><a href="/ReseltProject/shop/shopDetail?pid='
+               +priceRange+' '
+               +items.gender_name+'" data-tags="'
+               +items.brand_name+'"><button type="button"><a href="/ReseltProject/shop/shopDetail?pid='
                +items.pid+'"><div class="bg-[#ebf0f4] w-60 h-60 rounded-xl"><img class="w-full object-contain min-h-0 h-full" src="http://3.39.241.175:6753/upload/resources/img/product/'
                +items.pid+'/'
                +items.img_file+'"></div><p class="text-left text-[16px] font-bold font-notoSans pl-1">'
                +items.brand_name+'</p><div class="h-20 w-60"><p class="text-left text-[14px] pl-1">'
                +items.product_name_eng+'</p><p class="text-left text-[13px] text-slate-400 pl-1" >'
                +items.product_name_kor+'</p></div><p class="item-price text-left text-[16px] font-bold font-notoSans">&nbsp;'
-               +items.released_price+'원'+'</p><p class="text-left text-[11px] text-slate-400">&nbsp;&nbsp;즉시 구매가</p></a></button><a href="#"><div class="h-6 w-24 grid grid-cols-4 content-start"><button id="bookmark" class="w-1 h-1 py-1"  data-modal-toggle="popup-modal"><svg id="bookmarkColor" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.25"><path stroke-linecap="round" stroke-linejoin="round" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" /></svg></button></a><p class="text-[12px] px-0 py-2" id="product_bookmark">'
+               +items.released_price.toLocaleString('en-US')+'원'+'</p><p class="text-left text-[11px] text-slate-400">&nbsp;&nbsp;즉시 구매가</p></a></button><a href="#"><div class="h-6 w-24 grid grid-cols-4 content-start"><button id="bookmark" class="w-1 h-1 py-1"  data-modal-toggle="popup-modal"><svg id="bookmarkColor" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.25"><path stroke-linecap="round" stroke-linejoin="round" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" /></svg></button></a><p class="text-[12px] px-0 py-2" id="product_bookmark">'
                +items.product_bookmark+'</p><a href="/ReseltProject/style/styleList"><button id="smile" class="w-1 h-1 py-1"><svg id="smileColor" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" click:viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.25" href="ReseltProject/"><path stroke-linecap="round" stroke-linejoin="round" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg></button></a><p class="text-[12px] px-0 py-2" id="product_likes">'
                +items.product_likes+'</p></div>')
                .appendTo($('#productList'));
                //brand_name ~ product_likes까지 div 처리 => right top filter ==> 삭제함
-               //.toLocaleString('en-US')
+         
                console.log(items.category_name_eng);
             });//each
 
@@ -102,21 +104,47 @@ var items = document.querySelectorAll('.product')
   
 Array.from(items).sort(function(a, b) {
     // using ~~ to cast the value to a number instead of a string
-    a = ~~a.querySelector('.item-price').innerText
-    b = ~~b.querySelector('.item-price').innerText
+    a = ~~a.document.getElementById('.item-price').innerText
+    b = ~~b.document.getElementById('.item-price').innerText
     return a - b
   }).forEach(function(n, i) {
     n.style.order = i
   })
 }
-// ajax상에 locale $ 잡아놓은 것 때문에 innerText안되는지 확인해볼것
 
 sortByNameBtn.addEventListener('click', sortingByName);
 sortByPriceBtn.addEventListener('click', sortByPriceLow);
 
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>> 04. Header Filter
+const items = document.getElementsByClassName("item"); 
+//window.location.href.includes('design') 
+
+showTag = (event, tag) => {
+// console.log(window.location.hash)
+	console.log('showing... ', tag)
+	for(let i = 0; i< items.length; i++){
+	  if(items[i].dataset.tags.includes(tag)){
+	    items[i].style.display = "block";
+	  }else{
+	    items[i].style.display = "none";
+	  	}
+      }
+}
+
+document.getElementById("legoBtn").addEventListener("click", (event) => showTag(event, 'Lego'));
+document.getElementById("newbalBtn").addEventListener("click", (event) => showTag(event, 'New Balance'));
+document.getElementById("chanelBtn").addEventListener("click", (event) => showTag(event, 'Chanel'));
+document.getElementById("yashiroBtn").addEventListener("click", (event) => showTag(event, 'Yashiro'));
+document.getElementById("rolexBtn").addEventListener("click", (event) => showTag(event, 'Rolex'));
+document.getElementById("helinoxBtn").addEventListener("click", (event) => showTag(event, 'Helinox'));
+document.getElementById("wooyoungmiBtn").addEventListener("click", (event) => showTag(event, 'Wooyoungmi'));
+document.getElementById("nikeBtn").addEventListener("click", (event) => showTag(event, 'NIKE'));
+document.getElementById("dysonBtn").addEventListener("click", (event) => showTag(event, 'Dyson'));
+document.getElementById("iabstudioBtn").addEventListener("click", (event) => showTag(event, 'IAB STUDIO'));
+
 	
-	
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>> 4. paging each itembox//
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>> 05. paging each itembox//
 function paging(){
    const page = document.getElementById("pg");
    console.log("page value = "+page.value);
@@ -124,7 +152,7 @@ function paging(){
 }
 
 
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>> 5. bookmark login getSession//
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>> 06. bookmark login getSession//
 $('#popup-modal').click(function(){
    $.ajax({
       type: 'post',
